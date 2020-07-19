67c0dffeb21b41b9be60c065ad0e06f46311f8af
therajdeepbiswas <r4jdeepbiswas@gmail.com>
2020-07-19 15:04:33 +0530

    initial commit with readme, textlist and pyfile

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # gtav-bcrew-check
+
+A python commandline tool that tells you if an RGSC user is in any of the banned crews listed as per HALPGTA.  
+
+Usage: ```python bcrewcheck.py space\_separated\_crewnames```  
+Where rgsc\_username is what appears in the URL in the form of https://socialclub.rockstargames.com/member/rgsc_username    
---
+++ bcrewcheck.py
@@ -0,0 +1,24 @@
+import sys
+
+if len(sys.argv) < 2:
+	print('Usage: python bcrewcheck.py space_separated_crewnames')
+
+bcrews = []
+
+with open('bannedcrewlist.txt', 'r') as bcrewlist:
+	for bcrew in bcrewlist:
+		bcrews.append(bcrew.strip())
+		
+inbcrews = []
+
+for crew in sys.argv[1:]:
+	if crew in bcrews:
+		inbcrews.append(crew)
+		
+if not inbcrews:
+	print("User is not in any of the banned crews.")
+	
+else:
+	print("User is in the following banned crews: ")
+	for crew in inbcrews:
+		print(crew)
